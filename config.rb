247260938500b56c334f8b8b8055a3680084ef1a
Rachel Nabors <rachel@thepinkcrow.com>
2013-05-21 12:25:54 -0700

    adding git ignore

---
+++ config.rb
@@ -0,0 +1,12 @@
+# Require any additional compass plugins here.
+require 'animation'
+
+# Set this to the root of your project when deployed:
+http_path = ""
+css_dir = ""
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+# To enable relative paths to assets via compass helper functions. Uncomment:
+ relative_assets = true
+output_style = :compressed
